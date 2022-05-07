3435c649e2bd8885f7509a731f5afa6ca5149714
hallcode <alexhall93@me.com>
2022-05-07 02:10:10 +0100

    Added react components

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,7 @@ import { sass } from '@stencil/sass';
 import { postcss } from '@stencil/postcss';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
+import { reactOutputTarget as react } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'ptu-elements',
@@ -14,6 +15,11 @@ export const config: Config = {
     }),
   ],
   outputTargets: [
+    react({
+      componentCorePackage: 'ptu-elements',
+      proxiesFile: '../ptu-elements/src/components/stencil-generated/index.ts',
+      includeDefineCustomElements: true,
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
